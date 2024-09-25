8f5c4c947e2957c3dd7e3911b5f655f7f9c834e0
gulkovska <gylkovskasasha@icloud.com>
2024-09-25 15:53:01 +0300

    Added the class "List".

--- ooop/lab1.cpp
+++ ooop/lab1.cpp
@@ -6,10 +6,71 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+ 
+// Однозв'язний список
+template<typename T>
+class List
+{
+public:
+    List();
+    ~List();
+    void pop_front(); // Видалення першого елемента списку
+    void clear(); // Очищення всього списку
+    
+private:
+    
+    class Node
+    {
+    public:
+        Node *pNext;
+        T data;
+        
+        Node(T data = T(), Node *pNext = nullptr){
+            this->data = data;
+            this->pNext = pNext;
+        }
+    };
+    int Size;
+    Node *head;
+};
+
+// Конструктор
+template<typename T>
+List<T>::List(){
+    Size = 0;
+    head = nullptr;
+}
+
+// Деструктор
+template<typename T>
+List<T>::~List(){
+    clear(); // Видаляємо всі елементи
+}
+
+// Очищення списку
+template<typename T>
+void List<T>::clear(){
+    while (Size)
+        pop_front();
+}
+
+// Видалення першого елемента
+template<typename T>
+void List<T>::pop_front(){
+    if (head != nullptr) {
+        Node* temp = head; // Зберігаємо адресу першого елемента
+        head = head->pNext; // Зсуваємо голову до наступного елемента
+        delete temp; // Видаляємо стару голову
+        Size--;
+    }
+}
+
 int main()
 {
+    List<int> lst;
     return 0;
 }
