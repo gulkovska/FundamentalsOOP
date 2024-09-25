cb46a5aaa4d732832cb27e9e58fcabfa403131e8
gulkovska <gylkovskasasha@icloud.com>
2024-09-25 15:21:41 +0300

    Initial Commit

---
+++ ooop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ooop
+//
+//  Created by Sasha Gylkovska on 25.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
