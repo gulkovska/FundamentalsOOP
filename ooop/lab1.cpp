1b2fa9530f9ec479642850a1d5abe55f555aada7
gulkovska <gylkovskasasha@icloud.com>
2024-09-26 11:18:05 +0300

    added functions: push back, push front, insert, remove at, pop back.

--- ooop/lab1.cpp
+++ ooop/lab1.cpp
@@ -20,7 +20,15 @@ public:
     ~List();
     void pop_front(); // Видалення першого елемента списку
     void clear(); // Очищення всього списку
+    void push_back(T data); // Вставка в кінець списку
+    int GetSize() { return Size; } // Отримання розміру списку
+    T& operator[](const int index); // Доступ до елемента за індексом
     
+    void push_front(T data);// вставка в початок
+    void insert(T value, int index); // вставка
+    void removeAt(int index);//видаляє елемент за вказаним індексом у списку.
+    void pop_back();//видаляє останній елемент у списку.
+
 private:
     
     class Node
@@ -48,7 +56,7 @@ List<T>::List(){
 // Деструктор
 template<typename T>
 List<T>::~List(){
-    clear(); // Видаляємо всі елементи
+    clear();  
 }
 
 // Очищення списку
@@ -62,15 +70,162 @@ void List<T>::clear(){
 template<typename T>
 void List<T>::pop_front(){
     if (head != nullptr) {
-        Node* temp = head; // Зберігаємо адресу першого елемента
-        head = head->pNext; // Зсуваємо голову до наступного елемента
-        delete temp; // Видаляємо стару голову
+        Node* temp = head;
+        head = head->pNext;
+        delete temp;
         Size--;
     }
 }
 
+// Додавання елемента в кінець списку
+template<typename T>
+void List<T>::push_back(T data)
+{
+    if (head == nullptr){
+        head = new Node(data);
+    }
+    else{
+        Node* current = this->head;
+        while(current->pNext != nullptr){
+            current = current->pNext;
+        }
+        current->pNext = new Node(data);
+    }
+    Size++;
+}
+
+ 
+
+// Оператор доступу до елемента за індексом
+template<typename T>
+T& List<T>::operator[](const int index){
+    int counter = 0;
+    Node* current = this->head;
+    while (current != nullptr){
+        if (counter == index){
+            return current->data;
+        }
+        current = current->pNext;
+        counter++;
+    }
+    throw out_of_range("Індекс за межами списку");
+}
+
+// вставка елемента на початок
+template<typename T>
+void List<T>::push_front(T data){
+    head = new Node(data, head);
+    Size++;
+}
+
+
+// Вставка елемента за індексом
+template<typename T>
+void List<T>::insert(T data, int index){
+    if (index==0){
+        push_front(data);
+    }
+    else{
+        Node *previous = this->head;
+        for( int i = 0 ; i<index-1; i++){
+            previous = previous->pNext;
+        }
+        Node *newNode = new Node(data, previous->pNext);
+        previous->pNext = newNode;
+        Size++;
+    }
+    
+}
+
+// Видалення елемента за індексом
+    template<typename T>
+    void List<T>::removeAt(int index)
+{
+        if(index==0)
+        {
+            pop_front();
+        }
+        else
+        {
+            Node *previous = this->head;
+            for(int i = 0; i<index-1; i++)
+            {
+                previous = previous->pNext;
+            }
+            
+            Node *toDelete = previous->pNext;
+            previous->pNext = toDelete->pNext;
+            delete toDelete;
+            
+            Size--;
+        }
+}
+// Видалення останнього елемента зі списку
+template<typename T>
+void List<T>::pop_back()
+{
+    removeAt(Size-1);
+}
+
+
 int main()
 {
     List<int> lst;
+    
+    // Додавання елементів
+    lst.push_back(10);
+    lst.push_back(20);
+    lst.push_back(30);
+    cout << "Елементи після додавання в кінець: ";
+    for (int i = 0; i < lst.GetSize(); i++) {
+        cout << lst[i] << " ";
+    }
+    cout << endl;
+
+    // Додавання на початок
+    lst.push_front(5);
+    cout << "Елементи після додавання на початок: ";
+    for (int i = 0; i < lst.GetSize(); i++) {
+        cout << lst[i] << " ";
+    }
+    cout << endl;
+
+    // Вставка за індексом
+    lst.insert(15, 2);
+    cout << "Елементи після вставки на 2-й індекс: ";
+    for (int i = 0; i < lst.GetSize(); i++) {
+        cout << lst[i] << " ";
+    }
+    cout << endl;
+
+    // Видалення елемента за індексом
+    lst.removeAt(2);
+    cout << "Елементи після видалення 2-го індексу: ";
+    for (int i = 0; i < lst.GetSize(); i++) {
+        cout << lst[i] << " ";
+    }
+    cout << endl;
+
+    // Видалення першого елемента
+    lst.pop_front();
+    cout << "Елементи після видалення першого елемента: ";
+    for (int i = 0; i < lst.GetSize(); i++) {
+        cout << lst[i] << " ";
+    }
+    cout << endl;
+
+    // Видалення останнього елемента
+    lst.pop_back();
+    cout << "Елементи після видалення останнього елемента: ";
+    for (int i = 0; i < lst.GetSize(); i++) {
+        cout << lst[i] << " ";
+    }
+    cout << endl;
+
+//Очищення списку
+  lst.clear();
+   cout << "Розмір після очищення: " << lst.GetSize() << endl;
+
+    
     return 0;
 }
