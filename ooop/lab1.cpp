437b97338e19d80224746de9acfe9ec66887114b
gulkovska <gylkovskasasha@icloud.com>
2024-09-27 15:50:45 +0300

    added "mergeSort"

--- ooop/lab1.cpp
+++ ooop/lab1.cpp
@@ -594,6 +594,16 @@ public:
         }
     }
     
+    // Злиття для сортування злиттям
+    void mergeSort(int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(left, mid);
+            mergeSort(mid + 1, right);
+            merge(left, mid, right);
+        }
+    }
+    
 private:
     vector<T>& arr;
     
@@ -611,6 +621,45 @@ private:
         swap(arr[i + 1], arr[high]);
         return i + 1;
     }
+    
+    // Допоміжна функція для сортування злиттям
+    void merge(int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        vector<T> L(n1), R(n2);
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int i = 0; i < n2; i++)
+            R[i] = arr[mid + 1 + i];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    
 };
 
 
@@ -837,12 +886,21 @@ int main()
        }
        cout << endl;
 */
-    sorter.quicksort(0, data.size() - 1);
+   /* sorter.quicksort(0, data.size() - 1);
     cout << "Масив після швидкого сортування: ";
     for (int num : data) {
         cout << num << " ";
     }
     cout << endl;
+    */
+    
+    sorter.mergeSort(0, data.size() - 1);
+    cout << "Масив після сортування злиттям: ";
+    for (int num : data) {
+        cout << num << " ";
+    }
+    cout << endl;
+     
     
     return 0;
 }
